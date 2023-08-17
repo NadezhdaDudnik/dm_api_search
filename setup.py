7e47206acf2ec403fcc80f04f3f768276848a560
nadin869 <nady86.86@mail.ru>
2023-08-18 00:17:28 +0300

    Добавлен пакет dm_api_search

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+REQUIRES = [
+    'grpc',
+    'structlog',
+    'google',
+]
+
+setup(
+    name='dm_api_search',
+    version='0.0.1',
+    packages=['dm_api_search'],
+    url='https://github.com/NadezhdaDudnik/dm_api_search.git',
+    license='MIT',
+    author='Nadezhda Dudnik',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='dm_api_search with looger and gRPC'
+)
